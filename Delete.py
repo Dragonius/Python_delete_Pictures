8da1c2916814ebb8d7b26abc7d6395b1050419eb
Jari Luukkonen <Dragonius@users.noreply.github.com>
2019-09-09 11:37:48 +0300

    Update Delete.py

--- Delete.py
+++ Delete.py
@@ -2,11 +2,29 @@ import sys
 import os
 import imageio
 import numpy as np
+from PIL import Image
 
 def main():
     filename = sys.argv[-1]
 
+    try:
+        im = Image.load(filename)
+        im.verify() #I perform also verify, don't know if he sees other types o defects
+        im.close() #reload is necessary in my case
+        im = Image.load(filename)
+        im.transpose(PIL.Image.FLIP_LEFT_RIGHT)
+        im.close()
+    except:
+    #manage excetions here
+        sys.exit(0)
+
     f = imageio.imread(filename, as_gray=True)
+    try:
+        imageio.verify(f)
+    # do stuff
+    except IOError:
+    # filename not an image file
+        sys.exit(0)
 
     def img_estim(img, thrshld):
         is_light = np.mean(img) > thrshld
