7d11e58c684bda0913424e5406086550f26ac37e
Dragonius <Blackerhawker@hotmail.com>
2021-06-02 12:43:23 +0300

    Added Multiprosessing

---
+++ webcam_delete_black_pic5.sh
@@ -0,0 +1,4 @@
+for i in /home/kayttaja/Kuvat/Webcam3/*.jpg; do python3 /home/kayttaja/Kuvat/Testaus_koodia/webcam_delete_prog_ver3.py "$i";done
+for i in /home/kayttaja/Kuvat/Webcam2/*.jpg; do python3 /home/kayttaja/Kuvat/Testaus_koodia/webcam_delete_prog_ver3.py "$i";done 
+for i in /home/kayttaja/Kuvat/Webcam1/*.jpg; do python3 /home/kayttaja/Kuvat/Testaus_koodia/webcam_delete_prog_ver3.py "$i";done
+for i in /home/kayttaja/Kuvat/Webcam/*.jpg;  do python3 /home/kayttaja/Kuvat/Testaus_koodia/webcam_delete_prog_ver3.py "$i";done
