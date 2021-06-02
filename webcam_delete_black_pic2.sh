5004c62d54e708973507658ca9e5dca138720c20
Dragonius <Blackerhawker@hotmail.com>
2021-06-02 08:43:01 +0300

    added new files to list to test

---
+++ webcam_delete_black_pic2.sh
@@ -0,0 +1,4 @@
+for i in /home/kayttaja/Kuvat/Webcam3/*.jpg; do python3 /home/kayttaja/Lataukset/Git/Python_delete_Pictures/webcam_delete_prog_ver2.py "$i" ;done
+for i in /home/kayttaja/Kuvat/Webcam2/*.jpg; do python3 /home/kayttaja/Lataukset/Git/Python_delete_Pictures/webcam_delete_prog_ver2.py "$i" ;done 
+for i in /home/kayttaja/Kuvat/Webcam1/*.jpg; do python3 /home/kayttaja/Lataukset/Git/Python_delete_Pictures/webcam_delete_prog_ver2.py "$i" ;done
+for i in /home/kayttaja/Kuvat/Webcam/*.jpg;  do python3 /home/kayttaja/Lataukset/Git/Python_delete_Pictures/webcam_delete_prog_ver2.py "$i" ;done
