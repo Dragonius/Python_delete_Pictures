c76eb74e9c560669833d7cf70878f8f06f129d7b
Dragonius <Blackerhawker@hotmail.com>
2022-07-15 16:09:38 +0300

    test functioning, no prints

--- delete3.py
+++ delete3.py
@@ -22,39 +22,39 @@ def main():
     for filename in os.listdir(path):
         print(path)
         if filename.endswith(".jpg"):
-            print(filename)
+        #    print(filename)
             pathfilename = path + filename
-            print(pathfilename)    
+        #    print(pathfilename)    
 
-    try:
-        im = Image.load(filename)
-        im.verify() #I perform also verify, don't know if he sees other types o defects
-        im.close() #reload is necessary in my case
-        im = Image.load(filename) 
-        im.transpose(PIL.Image.FLIP_LEFT_RIGHT)
-        im.close()
-    except: 
-    #manage excetions here
-        sys.exit(0)
+        try:
+            im = Image.load(filename)
+            im.verify() #I perform also verify, don't know if he sees other types o defects
+            im.close() #reload is necessary in my case
+            im = Image.load(filename) 
+            im.transpose(PIL.Image.FLIP_LEFT_RIGHT)
+            im.close()
+        except: 
+        #manage excetions here
+            sys.exit(0)
 
-    f = imageio.imread(filename, as_gray=True)
-    try:
-        imageio.verify(f)
-    # do stuff
-    except IOError:
-    # filename not an image file
-        sys.exit(0)
+        f = imageio.imread(filename, as_gray=True)
+        try:
+            imageio.verify(f)
+        # do stuff
+        except IOError:
+        # filename not an image file
+            sys.exit(0)
 
-    def img_estim(img, thrshld):
-        is_light = np.mean(img) > thrshld
-        #print(np.mean(img))
-        #return 'light' if is_light else os.remove(filename)
-        if is_light == 0:
-            os.remove(filename)
+        def img_estim(img, thrshld):
+            is_light = np.mean(img) > thrshld
+            #print(np.mean(img))
+            #return 'light' if is_light else os.remove(filename)
+            if is_light == 0:
+                os.remove(filename)
 
-    img_estim(f, 40)
-    #print(img_estim(f, 40))
-    #print
+        img_estim(f, 40)
+        #print(img_estim(f, 40))
+        #print
 
 if __name__ == '__main__':
     try:
