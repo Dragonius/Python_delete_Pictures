5acc6bb31f2acec05281b529b257f5985a7adb60
Dragonius <Blackerhawker@hotmail.com>
2022-08-12 18:53:22 +0300

    added if python version ccontrol import

--- test_delete.py
+++ test_delete.py
@@ -4,7 +4,12 @@ import sys
 import os
 #import imageio
 #Bellow import is getting ready to imageio V2 import when imageio v3 released
-import imageio.v2 as imageio
+#import imageio.v2 as imageio
+if sys.version_info < (3, 7):
+    import imageio
+else:
+    import imageio.v2 as imageio
+
 import numpy as np
 
 #missing all comments
