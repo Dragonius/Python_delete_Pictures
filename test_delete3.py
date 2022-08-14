8ed3b0e13bc8fc6553dad5bc332d7795880f6674
Dragonius <Blackerhawker@hotmail.com>
2022-08-14 15:03:45 +0300

    updateded test_delete3 but still not complete

--- test_delete3.py
+++ test_delete3.py
@@ -2,6 +2,8 @@
 
 import sys
 import os
+#import imageio
+#image io got updated and they building next to next v2 and v3 brances.
 if sys.version_info < (3, 7):
     import imageio
 else:
@@ -15,7 +17,7 @@ from PIL import Image
 
 # print the list
 #print(dir_list)
-
+#ask for arguments for directory, ifn't given any , exit
 
 def main():
     path = sys.argv[-1]
@@ -23,47 +25,89 @@ def main():
         os._exit(0)
 
     dir_list = os.listdir(path)
+    #print(dir_list)
     for filename in os.listdir(path):
         #print(path)
         if filename.endswith(".jpg"):
-        #    print(filename)
+            #print(filename)
+            #print(path)
             pathfilename = path + filename
-        #    print(pathfilename)
+            #print(pathfilename)
 
         #print(pathfilename)
         try:
-            im = Image.load(pathfilename)
-            im.verify() #I perform also verify, don't know if he sees other types o defects
-            im.close() #reload is necessary in my case
-            im = Image.load(pathfilename)
-            im.transpose(PIL.Image.FLIP_LEFT_RIGHT)
-            im.close()
+            img = Image.open(pathfilename)
+            img.verify() #I perform also verify, don't know if he sees other types o defects
+            img.close() #reload is necessary in my case
+#            img = Image.open(pathfilename)
+#            img.transpose(PIL.Image.FLIP_LEFT_RIGHT)
+#            img.close()
         except:
         #manage excetions here
-            print("Coultn't open image file on im")
+            print("Couldtn't open image file ", pathfilename , " on img ")
             #sys.exit(0)
+            #print(pathfilename)
 
         f = imageio.imread(pathfilename, as_gray=True)
+
+
+        #Imageio Verify is broken and its not coming back
         #print(pathfilename)
-        try:
+#        try:
             #print(pathfilename)
-            imageio.verify(f)
+#            imageio.verify(f)
         # do stuff
-        except IOError:
-        # filename not an image file
-            print("File is not image, IO error")
-            sys.exit(0)
+#        except IOError:
+#        # filename not an image file
+#            print("File is not image, IO error")
+#            sys.exit(0)
 
         def img_estim(img, thrshld):
             is_light = np.mean(img) > thrshld
             #print(np.mean(img))
             #return 'light' if is_light else os.remove(filename)
             if is_light == 0:
-                os.remove(pathfilename)
+                print("Delete this file")
+                #os.remove(pathfilename)
 
         img_estim(f, 40)
         #print(img_estim(f, 40))
         #print
+def test_imgislight_TRUE():
+#    imagetreshold = 141.1277
+    filename = "./test_files/Clouds_test.jpg"
+    f = imageio.imread(filename, as_gray=True)
+    is_light = np.mean(f) > 40
+    print(is_light)
+    assert is_light == True
+    
+
+def test_imagetresholdvalue_TRUE():
+     imagetresholdbig = 141.13
+     imagetresholdsmall = 141.11
+     filename = "./test_files/Clouds_test.jpg"
+     f = imageio.imread(filename, as_gray=True)
+     meanvalue1 = np.mean(f)
+     meanvalue = round(meanvalue1,5)
+     assert imagetresholdbig  >=  meanvalue >= imagetresholdsmall
+
+def test_imgislight_FALSE():
+#   imagetreshold = 141.1277
+    filename = "./test_files/Clouds_test2.jpg"
+    f = imageio.imread(filename, as_gray=True)
+    is_light = np.mean(f) > 40
+    print(is_light)
+    assert is_light == False
+
+
+def test_imagetresholdvalue_FALSE():
+     imagetresholdbig = 39.99
+     imagetresholdsmall = 1.00
+     filename = "./test_files/Clouds_test2.jpg"
+     f = imageio.imread(filename, as_gray=True)
+     meanvalue1 = np.mean(f)
+     meanvalue = round(meanvalue1,5)
+     assert imagetresholdbig  >=  meanvalue >= imagetresholdsmall
 
 if __name__ == '__main__':
     try:
@@ -74,4 +118,3 @@ if __name__ == '__main__':
             sys.exit(0)
         except SystemExit:
             os._exit(0)
-
