3136a229fc6c0abedafa724ee77d1e74b0abf381
Jari Luukkonen <Blackerhawker@hotmail.com>
2022-08-19 16:58:38 +0300

    added test file that list files on test_files folder and show file order

---
+++ test_python_list.py
@@ -0,0 +1,39 @@
+import sys
+import os
+#import imageio
+#image io got updated and they building next to next v2 and v3 brances.
+if sys.version_info < (3, 7):
+    import imageio
+else:
+    import imageio.v2 as imageio
+
+import numpy as np
+from PIL import Image
+
+
+#print("Files and directories in '", path, "' :")
+
+# print the list
+#print(dir_list)
+#ask for arguments for directory, ifn't given any , exit
+path = "/home/kayttaja/Lataukset/Git/Python_delete_Pictures/test_files/"
+dir_list = os.listdir(path)
+print(dir_list)
+for filename in os.listdir(path):
+    #print(path)
+    if filename.endswith(".jpg"):
+        #print(filename)
+        #print(path)
+        pathfilename = path + filename
+        print(pathfilename)
+
+data_len=len(dir_list)
+data_out = 0
+if (data_len > -1):
+    while (data_len > data_out):
+        
+        print(dir_list[data_out])
+        data_out= data_out+1
+        #wait(100)
+
+
