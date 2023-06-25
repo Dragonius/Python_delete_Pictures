8603dc2f5f599cac9231f81c6dde2724cb94937d
Jari Luukkonen <Blackerhawker@hotmail.com>
2023-06-25 12:16:52 +0300

    ifixed  mode L

--- test_delete.py
+++ test_delete.py
@@ -16,7 +16,7 @@ def main():
     #filename = "./test_files/Clouds_test.jpg"
     filename =  sys.argv[-1]
     #Bellow code is ImageIO Version 2
-    f = imageio.imread(filename, L)
+    f = imageio.imread(filename, pilmode='L')
     #bellow code is reading to imageio Version 3
     #f = iio.v3.imread(filename, as_gray=True)
     def img_estim(img, thrshld):
@@ -35,7 +35,7 @@ def main():
 def test_imgislight_TRUE():
     #imagetreshold = 141.1277
     filename = "./test_files/Clouds_test.jpg"
-    f = imageio.imread(filename, L)
+    f = imageio.imread(filename, pilmode='L')
     is_light = np.mean(f) > 40
     print(is_light)
     assert is_light == True
@@ -45,7 +45,7 @@ def test_imagetresholdvalue_TRUE():
     imagetresholdbig = 141.13
     imagetresholdsmall = 141.11
     filename = "./test_files/Clouds_test.jpg"
-    f = imageio.imread(filename, mode=F)
+    f = imageio.imread(filename, pilmode='L')
     meanvalue1 = np.mean(f)
     meanvalue = round(meanvalue1,5)
     assert imagetresholdbig  >=  meanvalue >= imagetresholdsmall
@@ -53,7 +53,7 @@ def test_imagetresholdvalue_TRUE():
 def test_imgislight_FALSE():
     #imagetreshold = 141.1277
     filename = "./test_files/Clouds_test2.jpg"
-    f = imageio.imread(filename, mode=F)
+    f = imageio.imread(filename, pilmode='L')
     is_light = np.mean(f) > 40
     print(is_light)
     assert is_light == False
@@ -63,7 +63,7 @@ def test_imagetresholdvalue_FALSE():
     imagetresholdbig = 39.99
     imagetresholdsmall = 1.00
     filename = "./test_files/Clouds_test2.jpg"
-    f = imageio.imread(filename, mode=F)
+    f = imageio.imread(filename, pilmode='L')
     meanvalue1 = np.mean(f)
     meanvalue = round(meanvalue1,5)
     assert imagetresholdbig  >=  meanvalue >= imagetresholdsmall
